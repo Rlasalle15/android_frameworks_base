da97c27395c1752f2caec0c983445a0e1172dfce
Sasikumar D R <sasikumardr@gmail.com>
2015-01-08 19:45:24 -0500

    Add Screen Record frameworks to the Power Menu with some other fixes

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -690,7 +690,6 @@ public final class ShutdownThread extends Thread {
     }
 
     private static void deviceRebootOrShutdown(boolean reboot, String reason) {
-<<<<<<< HEAD
         Class<?> cl;
         PathClassLoader oemClassLoader = new PathClassLoader("/system/framework/oem-services.jar",
             ClassLoader.getSystemClassLoader());
@@ -765,28 +764,5 @@ public final class ShutdownThread extends Thread {
             }
         }
     };
-=======
-       Class<?> cl;
-       PathClassLoader oemClassLoader = new PathClassLoader("/system/framework/oem-services.jar",
-                       ClassLoader.getSystemClassLoader());
-       String deviceShutdownClassName = "com.qti.server.power.ShutdownOem";
-       try{
-               cl = Class.forName(deviceShutdownClassName);
-               Method m;
-                       try {
-                               m = cl.getMethod("rebootOrShutdown", new Class[] {boolean.class, String.class});
-                               m.invoke(cl.newInstance(), reboot, reason);
-                       } catch (NoSuchMethodException ex) {
-                               Log.e(TAG, "rebootOrShutdown method not found in class " + deviceShutdownClassName);
-                       } catch (Exception ex) {
-                               Log.e(TAG, "Unknown exception hit while trying to invode rebootOrShutdown");
-                       }
-       } catch(ClassNotFoundException e) {
-               Log.e(TAG, "Unable to find class " + deviceShutdownClassName);
-       } catch (Exception e) {
-               Log.e(TAG, "Unknown exception while trying to invoke rebootOrShutdown");
-       }
-    }
->>>>>>> dedb34f... Add hook to call into oem shutdown function
 }
 
