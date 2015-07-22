aeef2b5435c294eb2eaf7f29d69153323bbe3688
d34d <clark@cyngn.com>
2015-07-22 12:22:09 -0700

    Only clear icon cache when theme/icon pack applied
    Icon cache would get cleared on boot when applying the user's theme
    at startup which can negatively impact launchers when they are loading
    icons.  Icon cache should persist as long as that icon pack is applied.
    
    Change-Id: I248e08cdc87a31c1718cbc53aed49891056300cc

--- services/core/java/com/android/server/ThemeService.java
+++ services/core/java/com/android/server/ThemeService.java
@@ -511,6 +511,7 @@ public class ThemeService extends IThemeService.Stub {
     }
 
     private boolean updateIcons(String pkgName) {
+        ThemeUtils.clearIconCache();
         try {
             if (pkgName.equals(SYSTEM_DEFAULT)) {
                 mPM.updateIconMaps(null);
